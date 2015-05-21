180235cda442c409f0494492c6161280b549bcd6
Pablo Mantellini <pmantellini@mulesoft.com>
2015-05-21 17:36:04 -0300

    improved watermark logic, moved custom filter to java transformer

--- src/main/java/org/mule/templates/FilterDuplicateUsersTransformer.java
+++ src/main/java/org/mule/templates/FilterDuplicateUsersTransformer.java
@@ -6,31 +6,20 @@
 
 package org.mule.templates;
 
-import org.apache.commons.lang.RandomStringUtils;
-import org.mule.api.MuleMessage;
-import org.mule.api.routing.filter.Filter;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-/**
- * The filter that's removing records from the payload with the same email address.
- *
- * @author aurel.medvegy
- */
-public class WorkersDeduplicationFilter
-        implements Filter {
-
-    @SuppressWarnings("unchecked")
+import org.apache.commons.lang.RandomStringUtils;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+ 
+public class FilterDuplicateUsersTransformer extends AbstractMessageTransformer{
+	@SuppressWarnings("unchecked")
     @Override
-    public boolean accept(MuleMessage message) {
-    	
-    	// Filters out payload with watermark only
-    	if (((List<Map<String, String>>) message.getPayload()).size() == 1)
-    		return false;
-
+    public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
         List<Map<String, String>> payload = (List<Map<String, String>>) message.getPayload();
         List<String> emails = new ArrayList<String>();
 
@@ -40,7 +29,7 @@ public class WorkersDeduplicationFilter
             Map<String, String> next = iterator.next();
             String email = next.get("Email");
 
-            if (emails.contains(email) || next.containsKey("LastModifiedDate")) {
+            if (emails.contains(email)) {
                 iterator.remove();
             } else {
                 emails.add(email);
@@ -48,6 +37,6 @@ public class WorkersDeduplicationFilter
             }
         }
 
-        return true;
+        return payload;
     }
 }
